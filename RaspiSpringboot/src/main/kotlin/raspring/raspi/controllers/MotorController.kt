c86c3b9319c4cb20c795f3cd0c645a4b4918eb85
Bill Tsapalos <littleprog@gmail.com>
2018-03-30 20:16:08 +0300

    Pin 7

--- RaspiSpringboot/src/main/kotlin/raspring/raspi/controllers/MotorController.kt
+++ RaspiSpringboot/src/main/kotlin/raspring/raspi/controllers/MotorController.kt
@@ -8,13 +8,13 @@ import com.pi4j.io.gpio.RaspiPin
 class MotorController{
     fun startMotor(){
         val gpio = GpioFactory.getInstance();
-        val pin8 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_08, "Pin8");
+        val pin7 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "Pin7");
         System.out.println("rotate motor clockwise for 3 seconds");
-        pin8.high();
+        pin7.high();
         Thread.sleep(6000);
         // stop motor
         System.out.println("Stopping motor");
-        pin8.low();
+        pin7.low();
         gpio.shutdown();
     }
 }

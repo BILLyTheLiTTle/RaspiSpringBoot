b89a520e43c00ba335fd7b5ad67950501c3aa6ba
Bill Tsapalos <littleprog@gmail.com>
2018-04-04 15:17:40 +0300

    Control motor with H bridge

--- RaspiSpringboot/src/main/kotlin/raspring/raspi/controllers/MotorController.kt
+++ RaspiSpringboot/src/main/kotlin/raspring/raspi/controllers/MotorController.kt
@@ -6,22 +6,27 @@ object MotorController{
     @Synchronized fun startMotor(){
         val gpio = GpioFactory.getInstance();
         val pin4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "Pin4")
-        val pin7 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "Pin7")
+        val pin7 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "Pin7")
+        val pin8 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "Pin8")
 
-        pin4.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
-        pin7.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+        pin4.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF)
+        pin7.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF)
+        pin8.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF)
 
         System.out.println("rotate motor clockwise for 6 seconds")
         pin4.high()
         pin7.high()
+        pin8.high()
         Thread.sleep(6000)
         // stop motor
         System.out.println("Stopping motor")
         pin4.low()
         pin7.low()
+        pin8.low()
         gpio.shutdown()
         gpio.unprovisionPin(pin4)
         gpio.unprovisionPin(pin7)
+        gpio.unprovisionPin(pin8)
         System.out.println("Stopped motor: ${gpio.isShutdown}")
 
         System.out.println("Setting to null")
